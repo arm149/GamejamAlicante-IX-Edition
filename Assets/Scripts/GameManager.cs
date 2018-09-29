2a71b1134df4a29f5196934bf667390b870488e5
Adrián Francés Lillo <adrianfranceslillo@gmail.com>
2018-09-29 23:09:53 +0200

    Added victory and defeat options

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckVictory();
+	}
+
+    private void CheckVictory()
+    {
+        GameObject[] g = GameObject.FindGameObjectsWithTag("Enemy");
+        Debug.Log("Entra: " + g.Length);
+        if(g.Length == 0)
+        {
+            //We win
+            Debug.Log("Hemos ganado");
+        }
+    }
+}
