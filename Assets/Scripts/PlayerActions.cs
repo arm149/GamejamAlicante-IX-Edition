7bab85d2f93c559f4c3cce844227d1d8c0e22018
Adrián Francés Lillo <adrianfranceslillo@gmail.com>
2018-09-29 10:53:38 +0200

    Added player statistics, flip, and jump action

---
+++ Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour {
+    //Force of the jump
+    public float JumpForce = 10;
+
+    //Is player jumping?
+    bool jumping = false;
+
+    //RigidBody2D component of the player
+    private Rigidbody2D rigidbody;
+
+    //Health of the player
+    private int CurrentHealth;
+
+    //Maximum health of the player
+    public int MaximumHealth = 3;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+        CurrentHealth = MaximumHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Jump"))
+        {
+            jumping = true;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        //Check if we are already jumping
+        if(jumping)
+        {
+            Jump();
+            jumping = false;
+        }
+    }
+
+    //Jump Action
+    void Jump()
+    {
+        //Add a vertical force to the player's rigidbody
+        rigidbody.AddForce(new Vector2(0f, JumpForce));
+    }
+
+    //Function to make damage to the player
+    void ReceiveDamage(int dmg)
+    {
+        //Check if the damage received is positive
+        if(dmg > 0)
+        {
+            //Make damage to the player
+            CurrentHealth -= dmg;
+            //Check if the player has died
+            if (CurrentHealth <= 0)
+            {
+                //Activate death state
+            }
+        }
+    }
+}
